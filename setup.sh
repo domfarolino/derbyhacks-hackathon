a47450102dd18ce1f7daa4a21a5baa96f7759307
Ishwar Agarwal <a.isshwar@gmail.com>
2016-04-09 19:24:42 +0000

    added setup files and gitignore

---
+++ setup.sh
@@ -0,0 +1,17 @@
+'''
+This file sets up the virtual environment. 
+Run "source setup.sh" each time you want to run the app. 
+'''
+
+mkdir -p data
+
+if [ ! -d venv ]
+then
+  virtualenv venv
+fi
+
+. venv/bin/activate
+
+pip install Flask
+pip install peewee
+pip install pyyaml
